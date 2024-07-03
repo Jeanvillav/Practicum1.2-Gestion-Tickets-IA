1974907c113a2987a18850c1a5cf2b517cf854be
Jeanvillav <115833094+Jeanvillav@users.noreply.github.com>
2024-07-03 15:33:54 -0500

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,58 +1,60 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.model_selection import train_test_split
+import tensorflow as tf
 import pickle
-
-# Verificación del uso de GPU
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-print("Is the GPU being used:", tf.config.list_physical_devices('GPU'))
 
 # Cargar el archivo CSV
 file_path = 'contactos2024_limpio.csv'
-
-# Intenta leer el archivo CSV con diferentes delimitadores
-try:
-    data = pd.read_csv(file_path, delimiter=',')
-except pd.errors.ParserError:
-    data = pd.read_csv(file_path, delimiter=';')
+# Cargar los datos
+data = pd.read_csv(file_path, delimiter=';')
 
 print(data.head())  # Mostrar las primeras filas del archivo CSV para verificar la carga correcta
 
 # Definir las categorías y sus prioridades
 category_priority = {
-    'Problemas con el pago': 1,
-    'Problemas con el viaje': 2,
-    'Problemas de seguridad': 1,
-    'Problemas con la cuenta/app': 2,
-    'Problemas de comportamiento': 2,
-    'Mensaje de operador': 3,
-    'Otros problemas': 3
+    'problemas con el pago': 'Inmediata',
+    'problemas con el viaje': 'Alta',
+    'problemas de seguridad': 'Inmediata',
+    'problemas con la cuenta/app': 'Alta',
+    'problemas de comportamiento': 'Urgente',
+    'mensaje de operador': 'Baja',
+    'otros problemas': 'Baja',
+    'objetos perdidos': 'Urgente',
+    'vocabulario inadecuado': 'Alta'
 }
 
 # Función para clasificar el mensaje y motivo
 def classify_message(message, motivo):
     combined_text = f"{motivo} {message}".lower()
-    
-    if any(keyword in combined_text for keyword in ["cobro", "quiere llevar sin taxímetro", "taxímetro", "cobró", "pagó", "costo de la carrera", "pagar", "pago", "cobrar", "justo"]):
-        return 'Problemas con el pago'
-    elif any(keyword in combined_text for keyword in ["nunca llegó", "no contestaba", "no contesta", "canceló solicitud", "nunca sali", "no atendió", "la unidad", "no quiso llevar", "no salió", "no respondió", "no sale", "no desea la carrera", "dirección indicado", "mala ubicación", "no responde", "tampoco contesta", "se fue", "otro taxi", "llevó otro cliente", "no espera", "hace pasar tiempo", "se ha ido", "dirección incorrecta", "se ido", "carrera no realizada", "otro lado", "atendió solicitud", "canceló solicitud", "finaliza la carrera"]):
-        return 'Problemas con el viaje'
-    elif any(keyword in combined_text for keyword in ["quiero ser conductor", "pre-registro", "quiero trabajar", "cómo ser conductor", "cómo trabajar", "trabajar en", "#estoydispuestoalcambio", "|", "#quieroserconductor"]):
-        return 'Mensaje de operador'
-    elif any(keyword in combined_text for keyword in ["inseguro", "seguridad", "inseguridad"]):
-        return 'Problemas de seguridad'
-    elif any(keyword in combined_text for keyword in ["contactar", "aplicativo", "eliminar cuenta", "código", "app", "registro", "cuenta", "problema con cuenta", "app no funciona"]):
-        return 'Problemas con la cuenta/app'
-    elif any(keyword in combined_text for keyword in ["asco de sujeto", "sexual", "uso teléfono hablo / chat", "actitud", "actitud desagradable", "desagradable", "mal cliente", "burla", "sexuales", "mal comportamiento"]):
-        return 'Problemas de comportamiento'
-    else:
-        return 'Otros problemas'
 
-# Clasificar los mensajes y motivos
+    if any(keyword in combined_text for keyword in ["taxímetro alterado", "cobro inadecuado", "cobro excesivo", "cobro", "taxímetro", "costo", "pagar", "pago"]):
+        return 'problemas con el pago'
+    elif any(keyword in combined_text for keyword in ["no contesta", "canceló solicitud", "ubicacion", "otro lado", "nunca llegó", "tiempo de espera largo", "no llegó a tiempo", "mal servicio", "vehículo incorrecto", "la unidad tardó demasiado tiempo", "llevó otro cliente", "la unidad no llegó", "vehículo desordenado", "no sale", "cancelo", "pesimo", "no desea la carrera", "no responde", "ya se", "no espera", "se ha ido", "seriedad", "carrera", "cancela", "esperando", "salió", "pide", "se fue", "doble", "sucio", "velocidad"]):
+        return 'problemas con el viaje'
+    elif any(keyword in combined_text for keyword in ["pre-registro", "#quieroserconductor", "#estoydispuestoalcambio"]):
+        return 'mensaje de operador'
+    elif any(keyword in combined_text for keyword in ["conducción peligrosa", "conducción brusca", "velocidad excesiva", "mal estado de vehículo", "teléfono hablo/chat", "etílico", "borracho", "mareado", "acompañante", "acompañado", "incorrecto", "peleando", "peliando", "loco"]):
+        return 'problemas de seguridad'
+    elif any(keyword in combined_text for keyword in ["app", "eliminar cuenta", "no me funciona de manera correcta", "verificación", "ubicación", "dirección"]):
+        return 'problemas con la cuenta/app'
+    elif any(keyword in combined_text for keyword in ["olvidó", "olvidado", "perdió", "olvidé", "se me cayó", "recuperar", "pérdida", "extravio", "olvido", "cayeron", "cayo", "perdida", "se me quedó", "se me quedo"]):
+        return 'objetos perdidos'
+    elif any(keyword in combined_text for keyword in ["desagradable", "comportamiento", "servicio poco amable", "actitud", "mal cliente", "acoso sexual", "agresivo", "exigente", "majadero", "prepotente", "grosero", "juega", "malcriado", "sexo", "grosero", "prepotente"]):
+        return 'problemas de comportamiento'
+    elif any(keyword in combined_text for keyword in ["hdp", "mmv", "vrg", "hp", "crvrg", "puta", "mrd", "verga", "hijo", "perra", "malparido", "pendejo", "caraverga", "gil"]):
+        return 'vocabulario inadecuado'
+    elif any(keyword in combined_text for keyword in ["bien", "excelente", "buen"]):
+        return 'otros problemas'
+    else:
+        return 'otros problemas'
+
+# Convertir todos los textos a minúsculas y clasificar los mensajes y motivos
+data['mensaje'] = data['mensaje'].str.lower()
+data['motivo'] = data['motivo'].str.lower()
 data['categoria'] = data.apply(lambda row: classify_message(row['mensaje'], row['motivo']), axis=1)
 
 # Preparar los datos
@@ -98,22 +100,6 @@ with open(tokenizer_path, 'wb') as f:
 
 # Guardar las clases del LabelEncoder
 np.save('classes.npy', label_encoder.classes_)
-
-# Desplegar el modelo para clasificar nuevos mensajes
-def classify_new_message(message, motivo):
-    combined_text = f"{motivo} {message}".lower()
-    seq = tokenizer.texts_to_sequences([combined_text])
-    pad = pad_sequences(seq, maxlen=max_length)
-    pred = model.predict(pad)
-    category = label_encoder.inverse_transform([np.argmax(pred)])
-    return category[0]
-
-# Ejemplo de uso
-new_message = "El conductor nunca llegó y no me contestaba los mensajes"
-new_motivo = "solicitud cliente"
-category = classify_new_message(new_message, new_motivo)
-priority = category_priority.get(category, 3)
-print(f'Categoría: {category}, Prioridad: {priority}')
 
 # Guardar el modelo
 model.save('modelo_ticketing.keras')
